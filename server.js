95f5385ebd1f0ca6a56fd3099159c7f6b94ef305
omni <omni.omniscience@gmail.com>
2025-08-03 20:38:22 +0200

    Ajout tire en ligne.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const wss = new WebSocketServer({ server });
 app.use(express.static('public'));
 
 const clients = new Map();
+const projectiles = new Map(); // id → { from, x, y, angle, createdAt }
+
 
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
@@ -60,6 +62,34 @@ wss.on('connection', (ws) => {
       }
     }
 
+  if (msg.type === 'shoot' && client.pseudo) {
+      const idProj = 'p-' + randomUUID();
+      const projectile = {
+        id: idProj,
+        from: client.id,
+        x: msg.x,
+        y: msg.y,
+        angle: msg.angle,
+        color: '#ffffff',
+        createdAt: Date.now()
+      };
+      projectiles.set(idProj, projectile);
+
+      const payload = {
+        type: 'projectile',
+        ...projectile
+      };
+
+      for (const other of clients.keys()) {
+        other.send(JSON.stringify(payload));
+      }
+
+      // auto-cleanup (1.5s)
+      setTimeout(() => {
+        projectiles.delete(idProj);
+      }, 1500);
+    }
+
   });
 
   ws.on('close', () => {
@@ -68,6 +98,6 @@ wss.on('connection', (ws) => {
 });
 
 server.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000');
+  console.log('Server listening on http://192.168.1.152:3000');
 });
 
