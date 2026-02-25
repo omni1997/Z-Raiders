e32108d52c9ded48531f13217a04862dceff0999
omni <omni.omniscience@gmail.com>
2026-02-26 00:42:31 +0100

    Ajout du sprit viseur.

--- public/gameScene.js
+++ public/gameScene.js
@@ -11,6 +11,7 @@ export class GameScene extends Phaser.Scene {
   preload() {
     this.load.image('map', 'assets/map.png');
     this.load.image('player', 'assets/player.png');
+    this.load.image('sight', 'assets/sight.png');
   }
 
   create() {
@@ -18,14 +19,12 @@ export class GameScene extends Phaser.Scene {
       .setOrigin(0)
       .setDepth(-1)
       .setDisplaySize(MAP_WIDTH, MAP_HEIGHT);
-
     this.physics.world.setBounds(0, 0, MAP_WIDTH, MAP_HEIGHT);
     this.cameras.main.setBounds(0, 0, MAP_WIDTH, MAP_HEIGHT);
-
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.graphics = this.add.graphics();
     this.projectiles = this.add.group();
-
+    this.sight = this.add.image(0, 0, 'sight').setDepth(10).setScale(0.5);
+    this.input.setDefaultCursor('none');
     this.input.on('pointermove', pointer => {
       if (this.player) {
         this.aimAngle = Phaser.Math.Angle.Between(
@@ -34,7 +33,6 @@ export class GameScene extends Phaser.Scene {
         );
       }
     });
-
     this.input.on('pointerdown', this.shoot, this);
   }
 
@@ -44,17 +42,13 @@ export class GameScene extends Phaser.Scene {
 
   update() {
     if (!this.player || !pseudo) return;
-
     let vx = 0, vy = 0;
     const speed = 200;
-
     if (this.cursors.left.isDown) vx = -speed;
     if (this.cursors.right.isDown) vx = speed;
     if (this.cursors.up.isDown) vy = -speed;
     if (this.cursors.down.isDown) vy = speed;
-
     this.player.body.setVelocity(vx, vy);
-
     if (vx !== 0 || vy !== 0) {
       socket.send(JSON.stringify({
         type: 'move',
@@ -62,30 +56,9 @@ export class GameScene extends Phaser.Scene {
         y: this.player.y,
       }));
     }
-
-    if (this.player && this.input.activePointer) {
-      const pointer = this.input.activePointer;
-      const worldPoint = this.cameras.main.getWorldPoint(pointer.x, pointer.y);
-
-      const startX = this.player.x;
-      const startY = this.player.y;
-      const dx = worldPoint.x - startX;
-      const dy = worldPoint.y - startY;
-      const length = Math.sqrt(dx * dx + dy * dy);
-      const maxLength = 100;
-      const ratio = Math.min(maxLength / length, 1);
-
-      const endX = startX + dx * ratio;
-      const endY = startY + dy * ratio;
-
-      this.graphics.clear();
-      this.graphics.lineStyle(4, 0xff0000, 1);
-      this.graphics.beginPath();
-      this.graphics.moveTo(startX, startY);
-      this.graphics.lineTo(endX, endY);
-      this.graphics.strokePath();
-    }
-
+    const pointer = this.input.activePointer;
+    const worldPoint = this.cameras.main.getWorldPoint(pointer.x, pointer.y);
+    this.sight.setPosition(worldPoint.x, worldPoint.y);
     this.updateCamera();
   }
 
