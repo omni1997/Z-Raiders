a533f30083d42a70c9f5aa0122b11184488a580d
omni <omni.omniscience@gmail.com>
2026-02-28 01:54:55 +0100

    Resize phaser.

--- public/main.js
+++ public/main.js
@@ -2,15 +2,24 @@ import { GameScene } from './gameScene.js';
 import './socketHandler.js';
 import './chat.js';
 
+const gameZone = document.getElementById('game-zone');
+
 const config = {
   type: Phaser.AUTO,
-  width: document.getElementById('game-zone').clientWidth,
-  height: document.getElementById('game-zone').clientHeight,
+  width: gameZone.clientWidth,
+  height: gameZone.clientHeight,
   parent: 'game-zone',
   physics: { default: 'arcade' },
   scene: [GameScene],
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 };
 
 window.addEventListener('load', () => {
-  new Phaser.Game(config);
+  const game = new Phaser.Game(config);
+  window.addEventListener('resize', () => {
+    game.scale.resize(gameZone.clientWidth, gameZone.clientHeight);
+  });
 });
